befefa1e2fc42ab071f518cd1f553a2c415c1653
Durdona Ibragimova <durdona.dev@gmail.com>
2023-12-07 20:28:52 -0500

    Create people data table in react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { peopleData } from "./data";
+
+const Person = (props) => {
+    return (
+        <tr>
+            <td>{props.firstName}</td>
+            <td>{props.lastName}</td>
+            <td>{props.age}</td>
+            <td>{props.profession}</td>
+            <td>{props.yearlySalary}</td>
+            <td>{props.yearlyTotalExpenses}</td>
+            <td>{props.creditScore}</td>
+            <td>{props.yearlySaving}</td>
+            <td>{props.averageMonthlyExpense}</td>
+        </tr>
+    );
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="container">
+            <h1>People Data</h1>
+            <table>
+                <tr>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Age</th>
+                    <th>Profession</th>
+                    <th>Yearly Salary</th>
+                    <th>Yearly Total Expenses</th>
+                    <th>Credit Score</th>
+                    <th>Yearly Saving</th>
+                    <th>Average Monthly Expense</th>
+                </tr>
+                {peopleData.map((person, idx) => {
+                    return (
+                        <Person
+                            key={idx}
+                            firstName={person.firstName}
+                            lastName={person.lastName}
+                            age={person.age}
+                            profession={person.profession}
+                            yearlySalary={person.finance.yearlySalary}
+                            yearlyTotalExpenses={
+                                person.finance.yearlyTotalExpenses
+                            }
+                            creditScore={person.finance.creditScore}
+                            yearlySaving={
+                                person.finance.yearlySalary -
+                                person.finance.yearlyTotalExpenses
+                            }
+                            averageMonthlyExpense={
+                                person.finance.yearlyTotalExpenses / 12
+                            }
+                        />
+                    );
+                })}
+            </table>
+        </div>
+    );
 }
 
 export default App;
